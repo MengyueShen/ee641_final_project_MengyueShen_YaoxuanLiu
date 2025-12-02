9fe75115cb2ff1d9e5a03c1b0606983343bc9714
YaoxuanL <yaoxuanl37@gmail.com>
2025-12-02 11:08:03 +0800

    Update train_full_pipeline.py

--- src/experiments/train_full_pipeline.py
+++ src/experiments/train_full_pipeline.py
@@ -17,7 +17,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 from src.models.text_to_image_gan import TextToImageGAN
 from src.schedulers.annealed import AnnealedScheduler
-from src.utils.datasets import CUB200Dataset, collate_fn
+from src.utils.datasets import COCODataset, collate_fn
 from src.utils.visualization import TrainingVisualizer
 
 
